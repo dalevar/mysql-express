00158b861f2c2dac8b2ad10d19b36a8c0a07e0c4
Saidil Mifdal <saidilmifdal15@gmail.com>
2023-11-04 15:09:24 +0800

    deploy

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,18 @@
-import { Sequelize } from "sequelize";
+const mysql = require("mysql2");
 
-const db = new Sequelize(
-  process.env.DB_DBNAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-export default db;
+pool.getConnection((err, conn) => {
+  if (err) console.log(err);
+  console.log("Connected successfully");
+});
+
+module.exports = pool.promise();
